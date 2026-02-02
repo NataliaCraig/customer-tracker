220e53843900d9871b9c6f5779d825d252142105
Natalia <nataliacraig@usf.edu>
2026-02-02 14:31:07 -0500

    console edits

--- cc_3.js
+++ cc_3.js
@@ -1,21 +1,29 @@
 let customers = [
     {"name": "Madison Capes",
-        "email": "MadiCappppes@gmail.com",
-        "purchases": ["Sunflowers, roses, tulips"],
+        "email": "Madicappppes@gmail.com",
+        "purchases": ["Sunflowers", "roses", "tulips"],
     },
 {"name": "Rossi Edds",
     "email": "Eddsrossi25@gmail.com",
-    "purchases":["Peonies, roses, hibiscus"],
+    "purchases":["Peonies", "roses", "hibiscus"],
 },
 {"name": "Kara Baker",
     "email": "Karbaksurfs@gmail.com",
-    "purchases": ["Orchid, sunflowers, lavender"],
+    "purchases": ["Orchid","sunflowers", "lavender"],
 },
 ]
 customers.push({
     "name": "Katie Potter",
     "email": "Katieepgym@hotmail.com",
-    "purchases": "Jasmine, lilacs, roses",
+    "purchases": ["Jasmine", "lilacs", "roses",]
 });
+let removedCustomer = customers.shift();
+console.log("After removing first customer:", customers);
+
+customers[0].email = "mmadison.capess@gmail.com";
+console.log(customers[0]);
+
+
+
 
 
