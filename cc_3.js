4dae96345c27af91395190fc41001c2c4aeb5d10
Natalia <nataliacraig@usf.edu>
2026-02-02 14:00:31 -0500

    added name,  email, purchases to js

--- cc_3.js
+++ cc_3.js
@@ -1,1 +1,15 @@
+let customers = [
+    {"name": "Madison Capes",
+        "email": "MadiCappppes@gmail.com",
+        "purchases": ["Sunflowers, roses, tulips"],
+    },
+{"name": "Rossi Edds",
+    "email": "Eddsrossi25@gmail.com",
+    "purchases":["Peonies, roses, hibiscus"],
+},
+{"name": "Kara Baker",
+    "email": "Karbaksurfs@gmail.com",
+    "purchases": ["Orchid, sunflowers, lavender"],
+},
+]
 
