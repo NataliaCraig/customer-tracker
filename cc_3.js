d4abd751699fd4c62559ae8519eccd854b7f936c
Natalia <nataliacraig@usf.edu>
2026-02-03 13:44:25 -0500

    loop

--- cc_3.js
+++ cc_3.js
@@ -17,12 +17,16 @@ customers.push({
     "email": "Katieepgym@hotmail.com",
     "purchases": ["Jasmine", "lilacs", "roses",]
 });
+
 let removedCustomer = customers.shift();
 customers[0].email = "Rossiedds2025@gmail.com";
 console.log("After removing first customer:", customers);
-
-
+// Rossi is now at index 0
 console.log(customers[0]);
+
+customers.forEach(customer => {
+  console.log(`${customer.name} | ${customer.email} | Purchases: ${customer.purchases.length}`);
+});
 
 
 
